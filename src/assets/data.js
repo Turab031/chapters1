e894ea8d8029c29e50a214d0aadb41ef0b8312ee
Turab031 <147339712+Turab031@users.noreply.github.com>
2025-10-04 00:14:54 +0530

    page1

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "Which device is required for an internet connection?",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    answer: 1
+  },
+  {
+    question: "Which language is used for web page structure?",
+    option1: "HTML",
+    option2: "CSS",
+    option3: "JavaScript",
+    option4: "Python",
+    answer: 1
+  },
+  {
+    question: "Which company developed the React library?",
+    option1: "Google",
+    option2: "Microsoft",
+    option3: "Facebook",
+    option4: "Amazon",
+    answer: 3
+  },
+  {
+    question: "What does CSS stand for?",
+    option1: "Creative Style Sheets",
+    option2: "Cascading Style Sheets",
+    option3: "Computer Style System",
+    option4: "Custom Style Syntax",
+    answer: 2
+  },
+  {
+    question: "Which of the following is a programming language?",
+    option1: "HTTP",
+    option2: "Java",
+    option3: "SQL",
+    option4: "FTP",
+    answer: 2
+  }
+];
