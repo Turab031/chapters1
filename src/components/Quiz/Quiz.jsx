f57b23d750e8808f0adec29f782a6a5cf90702da
Turab031 <147339712+Turab031@users.noreply.github.com>
2025-10-04 10:35:03 +0530

    completed

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,42 +1,80 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Quiz.css";
 import { data } from "../../assets/data";
 
 const Quiz = () => {
   let [index, setIndex] = useState(0);
-  let [question, setQuestion] = useState(data[index]);
-  let [lock,setLock] = useState(false)
-  const checkAns =(e,ans)=>{
-    if(lock==false){
-         if(question.answer==ans){
-        e.target.classList.add("correct")
-        setLock(true)
-    }
-    else{
-        e.target.classList.add("wrong")
-        setLock(true)
-    }
+  let [question, setQuestion] = useState(data[0]);
+  let [lock, setLock] = useState(false);
+  let [score, setScore] = useState(0);
 
-    }
-   
+  let option1 = useRef(null);
+  let option2 = useRef(null);
+  let option3 = useRef(null);
+  let option4 = useRef(null);
+  let option_array = [option1, option2, option3, option4];
 
-  }
+  const checkAns = (e, ans) => {
+    if (!lock) {
+      if (question.answer === ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore((prev) => prev + 1);
+      } else {
+        e.target.classList.add("wrong");
+        setLock(true);
+        option_array[question.answer - 1].current.classList.add("correct");
+      }
+    }
+  };
+
+  const next = () => {
+    if (lock === true) {
+      if (index + 1 < data.length) {
+        const newIndex = index + 1;
+        setIndex(newIndex);
+        setQuestion(data[newIndex]);
+        setLock(false);
+
+        // reset option classes
+        option_array.forEach((option) => {
+          option.current.classList.remove("wrong");
+          option.current.classList.remove("correct");
+        });
+      } else {
+        alert(`Quiz Completed! Your score: ${score}/${data.length}`);
+      }
+    } else {
+      alert("Please select an answer before moving on!");
+    }
+  };
+
   return (
-
     <div className="container">
       <h1>Quiz App</h1>
       <hr />
       <h2>
-        {index + 1}.{question.question}
+        {index + 1}. {question.question}
       </h2>
       <ul>
-        <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-        <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-        <li onClick={(e)=>{checkAns(e,3)}}>{question.option3} </li>
-        <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+        <li ref={option1} onClick={(e) => checkAns(e, 1)}>
+          {question.option1}
+        </li>
+        <li ref={option2} onClick={(e) => checkAns(e, 2)}>
+          {question.option2}
+        </li>
+        <li ref={option3} onClick={(e) => checkAns(e, 3)}>
+          {question.option3}
+        </li>
+        <li ref={option4} onClick={(e) => checkAns(e, 4)}>
+          {question.option4}
+        </li>
       </ul>
-      <button>next</button>
-      <div className="index">1 of 5 question</div>
+      <button onClick={next}>Next</button>
+      <div className="index">
+        {index + 1} of {data.length} questions
+      </div>
+      <div className="score">Score: {score}</div>
     </div>
   );
 };
